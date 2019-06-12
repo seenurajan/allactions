199a7caa9534049399fa9403e37ec83fabe18b22
seenu.r <seenuchitrarajan@gmail.com>
2019-06-12 14:50:51 +0530

    Commit message

--- Configuratioa.js
+++ Configuratioa.js
@@ -2,7 +2,7 @@ var Jasmine2HtmlReporter1 = require('protractor-jasmine2-html-reporter');
 exports.config = {
   //seleniumAddress: 'http://localhost:4444/wd/hub', //direce connect without start server
   
-  specs: ['Banking.js'],
+  specs: ['Webtable.js'],
   //specs: ['registration.js'], 
   /*multiCapabilities: [{
 	  'browserName': 'firefox'
